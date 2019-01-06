59ba1fa193b70bc78e7540a148ae7dd9bc00d2d8
Eduardo Paz <edupaz2@gmail.com>
2019-01-06 15:45:15 +0000

    fix: Revert requirements.txt
    The upgrade of the requirements has brought so many headaches. Stick to the plan.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -13,16 +13,17 @@ class TLClassifier(object):
         self.output_operation = self.graph.get_operation_by_name('import/final_result')
         self.labels = ['green', 'none', 'red', 'yellow']
         self.light_states = [TrafficLight.GREEN, TrafficLight.UNKNOWN, TrafficLight.RED, TrafficLight.YELLOW]
-        self.sess = tf.Session(graph=self.graph)
+        self.sess = tf.Session(graph=self.graph, config=tf.ConfigProto(log_device_placement=True))
 
     def load_graph(self, model_file):
-        graph = tf.Graph()
-        graph_def = tf.GraphDef()
+        with tf.device('/gpu:0'):
+            graph = tf.Graph()
+            graph_def = tf.GraphDef()
 
-        with open(model_file, "rb") as f:
-            graph_def.ParseFromString(f.read())
-        with graph.as_default():
-            tf.import_graph_def(graph_def)
+            with open(model_file, "rb") as f:
+                graph_def.ParseFromString(f.read())
+            with graph.as_default():
+                tf.import_graph_def(graph_def)
 
         return graph
 
@@ -39,10 +40,11 @@ class TLClassifier(object):
         """
         tt = time.time()
 
-        results = self.sess.run(self.output_operation.outputs[0], {self.input_operation.outputs[0]: image})
-        
+        with tf.device('/gpu:0'):
+            results = self.sess.run(self.output_operation.outputs[0], {self.input_operation.outputs[0]: image})
+
         results = np.squeeze(results)
         top_k = results.argsort()[-5:][::-1]
         label_idx = top_k[0]
-        rospy.logwarn("TLClassifier::get_classification: Label: {0}, TotalTime: {1}. Result: {2}".format(self.labels[label_idx], time.time()-tt, ['{0}:{1}'.format(self.labels[i],results[i]) for i in top_k]))
+        #rospy.logwarn("TLClassifier::get_classification: Label: {0}, TotalTime: {1}. Result: {2}".format(self.labels[label_idx], time.time()-tt, ['{0}:{1}'.format(self.labels[i],results[i]) for i in top_k]))
         return self.light_states[label_idx]
