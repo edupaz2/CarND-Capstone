19f9acdcdb28aa25c50335c86a61015b7c74e005
Eduardo Paz <edupaz2@gmail.com>
2019-01-24 06:38:29 +0000

    fix: resizing is just delaying the latency issue

--- ros/src/styx/bridge.py
+++ ros/src/styx/bridge.py
@@ -18,7 +18,6 @@ from PIL import Image as PIL_Image
 from io import BytesIO
 import base64
 import time
-import cv2
 
 import math
 
@@ -187,7 +186,7 @@ class Bridge(object):
         imgString = data["image"]
         image = PIL_Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        image_array = cv2.resize(image_array, dsize=(224,224))
+
         image_message = self.bridge.cv2_to_imgmsg(image_array, encoding="rgb8")
         self.publishers['image'].publish(image_message)
 
